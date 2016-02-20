ea180ec7b8e92099f660a39ca1e3b56b403fc12b
RobertRiachi <roberjr@riachi.com>
2016-02-20 17:01:30 -0500

    List added

--- Skwad/www/modules/Userlist/Userlist.js
+++ Skwad/www/modules/Userlist/Userlist.js
@@ -5,6 +5,14 @@ angular.module('skwad.userlist', [])
 
   .controller('UserlistCtrl', function($scope) {
 
+    var username = [
+      {"fullname": "Rob"},
+      {"fullname": "Gabe"},
+      {"fullname": "Shaker"}
+    ];
+
+
+    $scope.usernames = username;
 
 
 
