32106584c14a0be5f83c7de4bb2e1434ffde9099
Mathieu Diab <mathieudiab@web080054.wireless.mcgill.ca>
2016-02-20 17:33:36 -0500

    Added Accounts View

--- Skwad/www/app.js
+++ Skwad/www/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'skwad.username', 'skwad.userlist', 'skwad.settingsFactory', 'skwad.start'])
+angular.module('starter', ['ionic', 'skwad.username', 'skwad.userlist', 'skwad.settingsFactory', 'skwad.start', 'skwad.accounts'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -43,6 +43,11 @@ angular.module('starter', ['ionic', 'skwad.username', 'skwad.userlist', 'skwad.s
         url: '/username',
         templateUrl: 'modules/username/username.html',
         controller: 'UsernameCtrl'
+      })
+      .state('/accounts', {
+        url: '/accounts',
+        templateUrl: 'modules/accounts/accounts.html',
+        controller: 'AccountsCtrl'
       })
 
     $urlRouterProvider.otherwise('/userlist');
